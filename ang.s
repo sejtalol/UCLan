32b897460d7950b03e7c80218794f15d17513787
sejtalol <42396678@qq.com>
2019-01-29 21:09:57 +0800

    rm useless files

--- ang.s
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for number in {0..200..1}
-do
-	./ang $number
-done
-
-mv ./ang_t*.dat ./data/
-exit 0
--- energy.s
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for number in {0..200..1}
-do
-	./energy $number
-done
-
-mv ./energy_t*.dat ./data/
-exit 0
