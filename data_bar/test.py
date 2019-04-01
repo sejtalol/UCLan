0eec00d9076cc36fb0afeda4d22608264217f6ca
sejtalol <sejtalol@gmail.com>
2019-04-01 15:28:55 +0100

    update

---
+++ data_bar/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+filename='ptcls_info_t400.dat'
+file=open(filename,'r')
+x1=[]
+y1=[]
+vx1=[]
+vy1=[]
+vz1=[]
+m1=[]
+pe1=[]
+for line in file:
+    values = line.split()     #break each line into a list
+    if (len(values)!=7):
+       print(values)
+
