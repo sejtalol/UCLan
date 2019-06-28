32e18727bd7b51387ab2b149805ee8ed5fc93a3e
sejtalol <sejtalol@gmail.com>
2019-06-28 16:36:21 +0800

    IAU & cheatsheets

---
+++ IAU/pyxl.py
@@ -0,0 +1,22 @@
+# pip install --user python-docx
+# pip install --user openpyxl
+
+import openpyxl
+import docx
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+wb= openpyxl.load_workbook('user_new.xlsx')
+sheet = wb.get_sheet_by_name('Abstracts Information')
+doc = docx.Document()
+doc.styles['Normal'].font.name = u'Times New Roman'
+nrows = 169
+ncols = 5
+table = doc.add_table(rows=nrows, cols=ncols, style='Table Grid')
+for a in range(1,nrows+1):
+    for b in range(1,ncols+1):
+        fill = sheet.cell(row=a,column=b).value
+#       print('a=',a,'b=',b,'value=',fill)
+        if(fill):
+           table.cell(a-1,b-1).text = fill
+        else:
+           table.cell(a-1,b-1).text = u''
+doc.save('users.docx')
